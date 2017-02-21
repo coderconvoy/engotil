6652fc40a647ec8bd782cb73c7ba30d44df01b11
coderconvoy <thebeaky@psmail.net>
2017-02-21 19:27:36 +0000

    nomore adds

--- add.go
+++ add.go
@@ -1,12 +1,25 @@
 package engotil
 
-import "engo.io/engo/common"
+/*
+import (
+	"engo.io/ecs"
+	"engo.io/engo/common"
+)
+
+//Basic
+type BasicEntity struct {
+	ecs.BasicEntity
+}
+
+func (be *BasicEntity) GetBasicEntity() *ecs.BasicEntity {
+	return &be.BasicEntity
+}
 
 //Animation
 type AnimationComponent common.AnimationComponent
 
 func (ac *AnimationComponent) GetAnimationComponent() *common.AnimationComponent {
-	return ac.AnimationComponent
+	return ac
 }
 
 type AnimationSystem common.AnimationSystem
@@ -21,3 +34,10 @@ type CollisionSystem common.CollisionSystem
 func (cs *CollisionSystem) AddByInterface(c Collidable) {
 	cs.Add(c.GetBasicEntity(), c.GetCollisionComponent(), c.GetSpaceComponent())
 }
+
+//Render
+type RenderComponent common.RenderComponent
+
+func (ac *RenderComponent) GetRenderComponent() *common.RenderComponent {
+	return ac
+}*/
--- g_test.go
+++ g_test.go
@@ -1,18 +1,29 @@
 package engotil
 
 import (
+	"image/color"
 	"testing"
 
 	"engo.io/ecs"
+	"engo.io/engo/common"
 )
 
 type Anime struct {
-	ecs.BasicEntity
+	Basic
 }
 
 func Test_Add(t *testing.T) {
 
+	k := Anime{
+		BasicEntity:        BasicEntity{ecs.NewBasic()},
+		AnimationComponent: common.NewAnimationComponent([]Drawable{}, 0.1),
+		RenderComponent: RenderComponent{
+			Drawable: Triangle{},
+			Color:    color.Black,
+		},
+	}
 	a := AnimationSystem{}
-	a
+
+	a.AddByInterface(k)
 
 }
